efc48c9cf943b44d9aa7f3c191ffe060985dc22d
nuwan.gayantha <nuwan.gayantha@codewilling.com>
2024-12-28 08:01:41 -0500

    test2

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express()
 app.get('/', (req, res) => {
     res.json({
         status: 200,
-        message: "hello world nuwan"
+        message: "hello world nuwan test1"
     })
 })
 
